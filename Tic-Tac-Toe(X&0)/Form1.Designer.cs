df166372d10342d4f323df6e61911990983f5cae
Gheorghe-Cristian Manasia <134348863+Criti0@users.noreply.github.com>
2024-03-03 08:01:04 +0200

    Add project files.

---
+++ Tic-Tac-Toe(X&0)/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Tic_Tac_Toe_X_0_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.b33 = new System.Windows.Forms.Button();
+            this.b32 = new System.Windows.Forms.Button();
+            this.b31 = new System.Windows.Forms.Button();
+            this.b23 = new System.Windows.Forms.Button();
+            this.b22 = new System.Windows.Forms.Button();
+            this.b21 = new System.Windows.Forms.Button();
+            this.b13 = new System.Windows.Forms.Button();
+            this.b12 = new System.Windows.Forms.Button();
+            this.b11 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33091F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33576F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.b33, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.b32, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.b23, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.b22, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.b21, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.b13, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.b12, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.b11, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.b31, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(682, 653);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // b33
+            // 
+            this.b33.FlatAppearance.BorderSize = 5;
+            this.b33.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b33.Font = new System.Drawing.Font("Arial Rounded MT Bold", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b33.Location = new System.Drawing.Point(457, 437);
+            this.b33.Name = "b33";
+            this.b33.Size = new System.Drawing.Size(221, 211);
+            this.b33.TabIndex = 8;
+            this.b33.UseVisualStyleBackColor = true;
+            this.b33.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b32
+            // 
+            this.b32.FlatAppearance.BorderSize = 5;
+            this.b32.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b32.Font = new System.Drawing.Font("Arial Rounded MT Bold", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b32.Location = new System.Drawing.Point(230, 437);
+            this.b32.Name = "b32";
+            this.b32.Size = new System.Drawing.Size(221, 211);
+            this.b32.TabIndex = 7;
+            this.b32.UseVisualStyleBackColor = true;
+            this.b32.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b31
+            // 
+            this.b31.FlatAppearance.BorderSize = 5;
+            this.b31.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b31.Font = new System.Drawing.Font("Arial Rounded MT Bold", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b31.Location = new System.Drawing.Point(3, 437);
+            this.b31.Name = "b31";
+            this.b31.Size = new System.Drawing.Size(221, 211);
+            this.b31.TabIndex = 6;
+            this.b31.UseVisualStyleBackColor = true;
+            this.b31.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b23
+            // 
+            this.b23.FlatAppearance.BorderSize = 5;
+            this.b23.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b23.Font = new System.Drawing.Font("Arial Rounded MT Bold", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b23.Location = new System.Drawing.Point(457, 220);
+            this.b23.Name = "b23";
+            this.b23.Size = new System.Drawing.Size(221, 211);
+            this.b23.TabIndex = 5;
+            this.b23.UseVisualStyleBackColor = true;
+            this.b23.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b22
+            // 
+            this.b22.FlatAppearance.BorderSize = 5;
+            this.b22.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b22.Font = new System.Drawing.Font("Arial Rounded MT Bold", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b22.Location = new System.Drawing.Point(230, 220);
+            this.b22.Name = "b22";
+            this.b22.Size = new System.Drawing.Size(221, 211);
+            this.b22.TabIndex = 4;
+            this.b22.UseVisualStyleBackColor = true;
+            this.b22.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b21
+            // 
+            this.b21.FlatAppearance.BorderSize = 5;
+            this.b21.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b21.Font = new System.Drawing.Font("Arial Rounded MT Bold", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b21.Location = new System.Drawing.Point(3, 220);
+            this.b21.Name = "b21";
+            this.b21.Size = new System.Drawing.Size(221, 211);
+            this.b21.TabIndex = 3;
+            this.b21.UseVisualStyleBackColor = true;
+            this.b21.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b13
+            // 
+            this.b13.FlatAppearance.BorderSize = 5;
+            this.b13.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b13.Font = new System.Drawing.Font("Arial Rounded MT Bold", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b13.Location = new System.Drawing.Point(457, 3);
+            this.b13.Name = "b13";
+            this.b13.Size = new System.Drawing.Size(221, 211);
+            this.b13.TabIndex = 2;
+            this.b13.UseVisualStyleBackColor = true;
+            this.b13.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b12
+            // 
+            this.b12.FlatAppearance.BorderSize = 5;
+            this.b12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b12.Font = new System.Drawing.Font("Arial Rounded MT Bold", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b12.Location = new System.Drawing.Point(230, 3);
+            this.b12.Name = "b12";
+            this.b12.Size = new System.Drawing.Size(221, 211);
+            this.b12.TabIndex = 1;
+            this.b12.UseVisualStyleBackColor = true;
+            this.b12.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b11
+            // 
+            this.b11.FlatAppearance.BorderSize = 5;
+            this.b11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b11.Font = new System.Drawing.Font("Arial Rounded MT Bold", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b11.Location = new System.Drawing.Point(3, 3);
+            this.b11.Name = "b11";
+            this.b11.Size = new System.Drawing.Size(221, 211);
+            this.b11.TabIndex = 0;
+            this.b11.UseVisualStyleBackColor = true;
+            this.b11.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 653);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(350, 350);
+            this.Name = "Form1";
+            this.Text = "X 0";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button b11;
+        private System.Windows.Forms.Button b13;
+        private System.Windows.Forms.Button b12;
+        private System.Windows.Forms.Button b33;
+        private System.Windows.Forms.Button b32;
+        private System.Windows.Forms.Button b31;
+        private System.Windows.Forms.Button b23;
+        private System.Windows.Forms.Button b22;
+        private System.Windows.Forms.Button b21;
+    }
+}
+
---
+++ Tic-Tac-Toe(X&0)/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe_X_0_
+{
+    public partial class Form1 : Form
+    {
+        char aux = 'x';
+        short k = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void b11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ResetGame()
+        {
+            b11.Enabled = true; b11.Text = "";
+            b12.Enabled = true; b12.Text = "";
+            b13.Enabled = true; b13.Text = "";
+            b21.Enabled = true; b21.Text = "";
+            b22.Enabled = true; b22.Text = "";
+            b23.Enabled = true; b23.Text = "";
+            b31.Enabled = true; b31.Text = "";
+            b32.Enabled = true; b32.Text = "";
+            b33.Enabled = true; b33.Text = "";
+            tableLayoutPanel1.Enabled = true;
+            aux = 'x';
+            k = 0;
+        }
+        private void button_Click(object sender, EventArgs e)
+        {
+            DialogResult ans;
+            Button bt = sender as Button;
+            bt.Enabled = true;
+            bt.BackColor = Color.Orange;
+            if(aux=='x')
+            {
+                bt.Text= "X";
+                if((b11.Text == b12.Text && b12.Text == b13.Text && b12.Text !="") ||
+                   (b21.Text == b22.Text && b22.Text == b23.Text && b22.Text != "") ||
+                   (b31.Text == b32.Text && b32.Text == b33.Text && b32.Text != "") ||
+                   (b11.Text == b21.Text && b21.Text == b31.Text && b21.Text != "") ||
+                   (b12.Text == b22.Text && b22.Text == b32.Text && b22.Text != "")  ||
+                   (b13.Text == b23.Text && b23.Text == b33.Text && b23.Text != "") ||
+                   (b11.Text == b22.Text && b22.Text == b33.Text && b22.Text != "") ||
+                   (b13.Text == b22.Text && b22.Text == b31.Text && b22.Text != ""))
+                {
+                    ans = MessageBox.Show("Castigator este: " + aux.ToString().ToUpper());
+                    if (ans == DialogResult.OK)
+                    {   
+
+                        Application.Exit();
+                    }
+                    tableLayoutPanel1.Enabled = false;
+
+                }
+                else if (k == 8)
+                {
+
+                    ans = MessageBox.Show("Castigator este: " + aux.ToString().ToUpper());
+                    if (ans == DialogResult.OK)
+                    {
+
+                        Application.Exit();
+                    }
+                }
+                aux = 'o';
+            }
+            else if(aux=='o')
+            {
+                
+                bt.Text= "O";
+                if ((b11.Text == b12.Text && b12.Text == b13.Text && b12.Text != "") ||
+                   (b21.Text == b22.Text && b22.Text == b23.Text && b22.Text != "") ||
+                   (b31.Text == b32.Text && b32.Text == b33.Text && b32.Text != "") ||
+                   (b11.Text == b21.Text && b21.Text == b31.Text && b21.Text != "") ||
+                   (b12.Text == b22.Text && b22.Text == b32.Text && b22.Text != "") ||
+                   (b13.Text == b23.Text && b23.Text == b33.Text && b23.Text != "") ||
+                   (b11.Text == b22.Text && b22.Text == b33.Text && b22.Text != "") ||
+                   (b13.Text == b22.Text && b22.Text == b31.Text && b22.Text != ""))
+                {
+                    ans = MessageBox.Show("Castigator este: " + aux.ToString().ToUpper() );
+                    if (ans == DialogResult.OK)
+                    {
+
+                        Application.Exit();
+                    }
+                    tableLayoutPanel1.Enabled = false;
+                }
+                else if(k==8)
+                {
+                    ans = MessageBox.Show("Castigator este: " + aux.ToString().ToUpper() );
+                    if (ans == DialogResult.OK)
+                    {
+
+                        Application.Exit();
+                    }
+                }
+                aux = 'x';
+            }
+            k++;
+        }
+
+    }
+}
